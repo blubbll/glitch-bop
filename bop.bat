fcc1b159cb99325adfa3016dac59b527210151f3
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2020-06-16 09:41:43 +0200

    add missing perms

--- bop.bat
+++ bop.bat
@@ -3,8 +3,8 @@ title Glitchy
 cd /d %~dp0
 
 :loop
-deno run --unstable --allow-net --allow-env https://raw.githubusercontent.com/blubbll/glitch-bop/master/bot.js
-
+::deno run --unstable --allow-all https://raw.githubusercontent.com/blubbll/glitch-bop/master/bot.js
+deno run --unstable --allow-net --allow-env --allow-read --allow-write --allow-plugin bot.js.ts
 goto loop
 
 exit /b 0
