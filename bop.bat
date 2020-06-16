ba25c7db6358b9a7244e6dc5da9d7944d9d733b6
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2020-06-16 21:29:47 +0200

    stop popping up yo

--- bop.bat
+++ bop.bat
@@ -5,7 +5,7 @@ cd /d %~dp0
 :loop
 cls
 ::deno run --unstable --allow-all https://raw.githubusercontent.com/blubbll/glitch-bop/master/bot.js
-start "GLITCHY" glitchy_deno run --unstable --allow-net --allow-env --allow-read --allow-write --allow-plugin bot.js.ts
+start /min "GLITCHY" glitchy_deno run --unstable --allow-net --allow-env --allow-read --allow-write --allow-plugin bot.js.ts
 timeout /t 120
 taskkill /F /IM glitchy_deno.exe
 goto loop
