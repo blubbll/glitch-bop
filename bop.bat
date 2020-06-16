73b818c2c023e3ef5f4c8ea295aceaae178d7420
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2020-06-16 10:04:17 +0200

    Update bop.bat

--- bop.bat
+++ bop.bat
@@ -3,6 +3,7 @@ title Glitchy
 cd /d %~dp0
 
 :loop
+cls
 ::deno run --unstable --allow-all https://raw.githubusercontent.com/blubbll/glitch-bop/master/bot.js
 deno run --unstable --allow-net --allow-env --allow-read --allow-write --allow-plugin bot.js.ts
 goto loop
