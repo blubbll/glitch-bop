fbca3b6a74c28716d9b9d49dec320d3192c280ac
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2020-06-16 21:38:24 +0200

    syntax, comments

--- bop.bat
+++ bop.bat
@@ -1,12 +1,17 @@
 @echo OFF
 title Glitchy_master
+::go to current dir
 cd /d %~dp0
 
 :loop
 cls
-::deno run --unstable --allow-all https://raw.githubusercontent.com/blubbll/glitch-bop/master/bot.js
+::start deno webview script
 start /min "GLITCHY" glitchy_deno run --unstable --allow-net --allow-env --allow-read --allow-write --allow-plugin bot.js.ts
+::wait 2 mins
 timeout /t 120
+::kill the binary (sigh)
 taskkill /F /IM glitchy_deno.exe
+::ima do it again
 goto loop
+
 exit /b 0
